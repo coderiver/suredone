588aa08102a8a12a9ab673ce99831d5270cd3ba9
Splintex <godun.yevgen@gmail.com>
2014-02-03 10:11:36 +0200

    popup upd

--- js/common.js
+++ js/common.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
 
+	$(document).click(function() {
+        $(".js-window-wrap ").hide();
+        $(".js-overlay").hide();
+        $("body").removeClass("is-window-open");
+    });
+	var
+		window_wrap = $(".js-window-wrap");
+		overlay = $(".js-overlay");
+		window_close = $(".js-window-close");
+	window_wrap.find('.window').click(function(event){
+        event.stopPropagation();
+    });
+    window_close.on("click", function(){
+    	$(this).parents(".js-window-wrap").hide();
+    	$(".js-overlay").hide();
+        $("body").removeClass("is-window-open");
+    });
+
 });
+
