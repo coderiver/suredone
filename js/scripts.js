c1b1ad94286f4478b6c8a15e033984f6b01db30b
Splintex <godun.yevgen@gmail.com>
2014-02-13 13:13:08 +0200

    fixes part 1

--- js/scripts.js
+++ js/scripts.js
@@ -11423,13 +11423,22 @@ $(document).ready(function() {
 		window_wrap = $(".js-window-wrap");
 		overlay = $(".js-overlay");
 		window_close = $(".js-window-close");
+		popup_key = $(".js-popup-key");
+		body = $("body");
 	window_wrap.find('.window').click(function(event){
         event.stopPropagation();
     });
     window_close.on("click", function(){
     	$(this).parents(".js-window-wrap").hide();
-    	$(".js-overlay").hide();
-        $("body").removeClass("is-window-open");
+    	overlay.hide();
+        body.removeClass("is-window-open");
+    });
+    popup_key.on("click", function(){
+    	var popup_url = $(this).attr("data-popup");
+    	$("."+popup_url).show();
+    	body.addClass("is-window-open");
+    	overlay.show();
+    	return false;
     });
 
     function sticky() {
@@ -11456,6 +11465,47 @@ $(document).ready(function() {
     $(window).scroll(function(){
         sticky();
     });
+    $(window).resize(function(){
+        sidebar_mobile();
+    });
+    $(".js-hide-sidebar").on("click", function(){
+    	body.addClass("is-no-sidebar");
+    });
+    $(".js-show-sidebar").on("click", function(){
+    	body.removeClass("is-no-sidebar");
+    	body.removeClass("has-sidebar");
+	    $(".js-sidebar-overlay").fadeOut();
+    });
+    function sidebar_mobile() {
+    	if ($(window).width() <= 640) {
+	    	$(".js-show-sidebar").on("click", function(){
+		    	body.addClass("has-sidebar");
+		    	$(".js-sidebar-overlay").fadeIn();
+		    });
+		    $(".js-hide-sidebar").on("click", function(){
+		    	body.removeClass("has-sidebar");
+		    	$(".js-sidebar-overlay").fadeOut();
+		    });
+	    }
+    }
+    sidebar_mobile();
+
+    function accordion() {
+        $(".js-accord-key").click(function(){
+            if ($(this).hasClass("is-active")) {
+                $(this).removeClass("is-active");
+                $(this).parent().find(".js-accord-list").slideUp("fast");
+            }
+            else {
+                $(".js-accord-key").removeClass("is-active");
+                $(this).addClass("is-active");
+                $(".js-accord-list").slideUp("fast");
+                $(this).parent().find(".js-accord-list").slideDown("fast");
+            }
+            return false;
+        });
+    }
+    accordion();
 
 });
 
